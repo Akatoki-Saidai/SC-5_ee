7bf7cceb5911c46f3a82b1e329cf67afb1e507ba
DDD3H <40761694+DDD3H@users.noreply.github.com>
2021-06-19 17:05:43 +0900

    Update BMP180new.ino
    シリアル通信は115200でお願いします！

--- BMP180/BMP180new.ino
+++ BMP180/BMP180new.ino
@@ -27,7 +27,7 @@
 Adafruit_BMP085 bmp;
   
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
