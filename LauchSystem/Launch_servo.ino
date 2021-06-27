1232d046db10b899de8af32bff6394045ff7ca0a
yuto8128 <83899255+yuto8128@users.noreply.github.com>
2021-06-27 12:20:42 +0900

    Update Launch_servo.ino

--- LauchSystem/Launch_servo.ino
+++ LauchSystem/Launch_servo.ino
@@ -1,7 +1,6 @@
 #include <ESP_servo.h>
 
 ESP_servo servo1; //サーボのオブジェクト宣言:
-ESP_servo servo2;
 
 int lauchc = 33;            //トランジスタのピン番号の宣言
 int outputsecond = 5;       //点火時の9V電圧を流す時間，単位はsecond
@@ -15,9 +14,7 @@ void setup(){
     Serial2.write("TESTING: Serial communication\n");
 
     servo1.init(23,0); //pinとチャンネルの選択:
-    servo2.init(21,1);
     servo1.write(0); //初めの角度:
-    servo2.write(0);
 }
 
 void loop(){
@@ -30,24 +27,7 @@ void loop(){
             Serial2.write("WARNING: Press the y key to allow firing.\n");
 
             while(true){
-                if(Serial2.available()){
-                    char key = Serial2.read();
-
-                    if(key == 'a'){
-                        Serial2.write("PLANE ANGLE.\n");
-                        for (int pos2 = 0; pos2 <= 150; pos2 += 1) {
-                           servo2.write(pos2);
-                           delay(20);
-                        }
-                        Serial.write("LAUNCH ANGLE.\n");
-                        for (int pos1 = 0; pos1 <= 45; pos1 += 1) {
-                           servo1.write(pos1);
-                           delay(20);
-                        }
-                        
-                    }
-                }
-
+              
                 if(Serial2.available()){
                     char key = Serial2.read();
                     
@@ -67,13 +47,27 @@ void loop(){
                         Serial2.write("LAUCHING: 9V voltage is stop.\n");
                         digitalWrite(lauchc, LOW); //オフ
                         break;
-                    }else if (key == 'n'){
-                        Serial2.write("****** Ignition operation canceled ******\n");
+                    }
+
+                    else if (key == 'a'){  
+                        Serial2.write("LAUNCH ANGLE. \n");
+                         for (int pos = 0; pos <= 45; pos += 1) {            //打ち上げ角度を決める
+                           servo1.write(pos); 
+                           delay(30);             //時間はどうしよっか？
                         break;
                     }
                 }
+                    
+                    else if (key == 'n'){
+                        Serial2.write("****** Ignition operation canceled ******\n");
+                        break;
+                    }
+                    
             }
-        }else if (key == 'e') //緊急停止用
+        }
+        }
+        
+        else if (key == 'e') //緊急停止用
         {
             digitalWrite(lauchc, LOW);
             Serial2.write("WARNING: The EMERGENCY code has been entered\n");
