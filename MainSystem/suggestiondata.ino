21e74a45277e6479d3b1a67c8381f20fe655d3e3
AkariGoto <76420233+AkariGoto@users.noreply.github.com>
2021-07-11 16:08:02 +0900

    Update suggestiondata.ino

--- MainSystem/suggestiondata.ino
+++ MainSystem/suggestiondata.ino
@@ -11,10 +11,15 @@ char key = '0';
 #define SDA_MPU 25
 #define SCL_MPU 26
 #endif
-
 MPU9250_asukiaaa mySensor;
 float aX, aY, aZ, aSqrt;
 
+//for BMP180
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
+#define SDA_BMP 21
+#define SCL_BMP 22
 
 void setup() {
     Serial.begin(115200);
@@ -28,7 +33,6 @@ void setup() {
     while(!Serial);
     Serial.println("started");
     #ifdef _ESP32_HAL_I2C_H_ // For ESP32
-    Wire.begin(SDA_PIN, SCL_PIN);
     mySensor.setWire(&Wire);
     #endif
     mySensor.beginAccel();
@@ -39,6 +43,12 @@ void setup() {
     mySensor.magXOffset = -50;
     mySensor.magYOffset = -55;
     mySensor.magZOffset = -10;
+    
+    //for BMP180
+    if (!bmp.begin()) {
+        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+        while (1) {}
+    }
   }
 
 
@@ -84,9 +94,9 @@ void loop() {
                 
                 //フェーズ1  MPU9250使用  機体の傾きを測定
                 Serial.println("You are in the phase 1");
+                Wire.begin(SDA_MPU, SCL_MPU);
 
-
-                double TBD;       //加速度TBD以上でphase2に移行
+                double TBDa;       //加速度TBDを入力
                 uint8_t sensorId;
                 if (mySensor.readId(&sensorId) == 0) {
                     Serial.println("sensorId: " + String(sensorId));
@@ -103,6 +113,25 @@ void loop() {
                 phase = 2;
 
             case 2: //降下フェーズ
+                //フェーズ2  BMP180使用  加速度の移動平均を測定
+                Serial.println("You are in the phase 2");
+                double Alt[5];
+                double ALT;
+                double TBDh;//決めた高度を入力
+  
+                //高度について、5個のデータの移動平均を出す。
+                for(int i=0;;i++){     //高度のデータを配列に入れる。
+                    Alt[i] = bmp.readAltitude();
+  
+                    double Altsum = 0;   //五個のデータの合計値
+  
+                    //先に作った配列の中身の和を出して、移動平均を出す
+                    for(int k=i-5 ; k==i ; k++){
+                        Altsum = Altsum + Alt[k];
+                        ALT = Altsum/5
+                    }
+                    if(i>5 && ALT<TBDh) phase = 3;　//高度の移動平均が決定地よりも低かったらループを抜け出す 
+                }
 
             case 3: //分離フェーズ
                 Serial2.write("WARNING: The cut-para code has been entered.\n");
